e7462ffba6a3ac15466889b6b39f920a0178f845
Salman <salman9915189734@gmail.com>
2022-03-07 23:41:53 +0530

    hash password

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,6 +2,10 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcryptjs");
+var jwt = require("jsonwebtoken");
+
+const JWT_SECRET = "qwertyuiopasdfghjklzxcvbnm";
 
 //Create a User using : POST "/api/auth/createuser" . No Login Required.
 
@@ -26,13 +30,20 @@ router.post(
           .status(400)
           .json({ error: "user with this email already exists" });
       }
+      const salt = await bcrypt.genSalt(10);
+      secPass = await bcrypt.hash(req.body.password, salt);
+
       //Create a New User.
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
-      res.json(user);
+      const data = {
+        id: user.id,
+      };
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
     } catch (error) {
       //Catch Errors
       console.log(error.message);
