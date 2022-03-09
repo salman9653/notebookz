47bac425ed32c3542712f64a45c0eb89476f2a67
Salman <salman9915189734@gmail.com>
2022-03-09 17:07:32 +0530

    Navbar and Routes Added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import About from "./components/About";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className='App'>
-      <h1>NoteBook X</h1>
-    </div>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/About" element={<About />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
